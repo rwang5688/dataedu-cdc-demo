55cfe50718a022f4f6c70f0e6dbe9ed6483f17ae
Robert Wang <rwang5688@outlook.com>
2021-11-12 20:02:14 -0800

    add convert-delta-lake.py

---
+++ src/convert-delta-lake.py
@@ -0,0 +1,9 @@
+from delta import *
+from pyspark.sql.session import SparkSession
+
+
+spark = SparkSession \
+    .builder \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
